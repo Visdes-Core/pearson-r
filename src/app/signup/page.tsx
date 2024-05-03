ece36f6a5db35e8a5d90864614de29a37f26bcbf
Fathan <fathan.naufal@ui.ac.id>
2024-05-03 22:55:20 +0700

    add: signup and login page, tailwind config

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import SignUpModule from "@/modules/SignupModule";
+
 export default function SignUp() {
   return (
-    <div className="flex min-h-screen flex-col items-stretch bg-[#F8F8F8]">
-      
+    <div className="flex min-h-screen justify-center items-center bg-[#F8F8F8]">
+      <SignUpModule />
     </div>
   );
 }
