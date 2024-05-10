182b8e99b044f65c65d0e81a85953cbcd20a909b
Fathan <fathan.naufal@ui.ac.id>
2024-05-10 15:16:42 +0700

    add card details

--- src/app/matching/page.tsx
+++ src/app/matching/page.tsx
@@ -4,7 +4,7 @@ import MatchingModule from "@/modules/MatchingModule";
 
 export default function Matching() {
   return (
-    <div className="flex min-h-screen justify-center p-20 bg-[#F8F8F8] overflow-hidden">
+    <div className="flex min-h-screen justify-center p-20 bg-[#F8F8F8]">
       <MatchingModule />
     </div>
   );
--- src/modules/MatchingModule/index.tsx
+++ src/modules/MatchingModule/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import MahasiswaCard from "./module-elements/MahasiswaCard";
 import TinderCard from 'react-tinder-card'
+import Link from 'next/link';
 
 function MatchingModule() {
   const [lastDirection, setLastDirection] = useState<string>()
@@ -18,14 +19,26 @@ function MatchingModule() {
       <div className="flex justify-between items-center mb-24 text-lg">
         <p>MATCH<br/><span className='font-bold'>MAJOR</span></p>
         <div className="flex gap-32">
-          <button>Pesan</button>
+          <Link href={'/chat'}>
+            <button>Pesan</button>
+          </Link>
           <button className='font-semibold'>Match</button>
-          <button>Profil</button>
+          <Link href={'/profil'}>
+            <button>Profil</button>
+          </Link>
         </div>
         <p className='font-semibold text-red-500'>Log out</p>
       </div>
       <div className="flex justify-center">
-        <TinderCard className='absolute' onSwipe={(dir) => swiped(dir, 'Azmy')}
+        <TinderCard className='absolute' onSwipe={(dir:string) => swiped(dir, 'Azmy')}
+        onCardLeftScreen={() => outOfFrame('Azmy')} preventSwipe={['right', 'left']}>
+          <MahasiswaCard />
+        </TinderCard>
+        <TinderCard className='absolute' onSwipe={(dir:string) => swiped(dir, 'Azmy')}
+        onCardLeftScreen={() => outOfFrame('Azmy')} preventSwipe={['right', 'left']}>
+          <MahasiswaCard />
+        </TinderCard>
+        <TinderCard className='absolute' onSwipe={(dir:string) => swiped(dir, 'Azmy')}
         onCardLeftScreen={() => outOfFrame('Azmy')} preventSwipe={['right', 'left']}>
           <MahasiswaCard />
         </TinderCard>
