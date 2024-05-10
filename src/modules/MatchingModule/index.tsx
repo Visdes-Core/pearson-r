628880cde1a8b248ec24022be207e31d1eb9963c
Fathan <fathan.naufal@ui.ac.id>
2024-05-10 22:36:31 +0700

    matching interactive feature

--- src/modules/MatchingModule/index.tsx
+++ src/modules/MatchingModule/index.tsx
@@ -1,13 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import MahasiswaCard from "./module-elements/MahasiswaCard";
 import TinderCard from 'react-tinder-card'
 import Link from 'next/link';
+import axios from 'axios';
+import { MahasiswaType, UserType } from './interface';
+import { ToastContainer, toast } from 'react-toastify';
+import { UserRoundCheck } from 'lucide-react';
 
 function MatchingModule() {
   const [lastDirection, setLastDirection] = useState<string>()
+  const [users, setUsers] = useState<UserType[]>([]);
+  
+  const notify = (swipedUsername: string) => toast.info("You swiped " + swipedUsername + "!", {
+    icon: ({theme, type}) =>  <UserRoundCheck />
+  });
 
-  const swiped = (direction:string, nameToDelete:string) => {
-    console.log('removing: ' + nameToDelete + ' to ' + direction)
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_URL}/user/get/list`);
+        const data: UserType[] = response.data
+        const result = data.filter((user) => user.mahasiswa!==null); 
+        setUsers(result);
+      } catch (error: any) {
+        console.error('Error fetching users:', error.message);
+      }
+    }
+
+    fetchUsers();
+  }, []);
+  console.log(users)
+
+  const swiped = (direction:string, name:string) => {
+    if(direction == 'up') {
+      notify(name)
+    }
     setLastDirection(direction)
   }
 
@@ -30,19 +57,19 @@ function MatchingModule() {
         <p className='font-semibold text-red-500'>Log out</p>
       </div>
       <div className="flex justify-center">
-        <TinderCard className='absolute' onSwipe={(dir:string) => swiped(dir, 'Azmy')}
-        onCardLeftScreen={() => outOfFrame('Azmy')} preventSwipe={['right', 'left']}>
-          <MahasiswaCard />
-        </TinderCard>
-        <TinderCard className='absolute' onSwipe={(dir:string) => swiped(dir, 'Azmy')}
-        onCardLeftScreen={() => outOfFrame('Azmy')} preventSwipe={['right', 'left']}>
-          <MahasiswaCard />
-        </TinderCard>
-        <TinderCard className='absolute' onSwipe={(dir:string) => swiped(dir, 'Azmy')}
-        onCardLeftScreen={() => outOfFrame('Azmy')} preventSwipe={['right', 'left']}>
-          <MahasiswaCard />
-        </TinderCard>
+        {users.map((user: UserType, index) => (
+          <TinderCard
+            key={index}
+            className='absolute'
+            onSwipe={(dir) => swiped(dir, user.email)}
+            onCardLeftScreen={() => outOfFrame(user.email)}
+            preventSwipe={['right', 'left']}
+          >
+            <MahasiswaCard user={user} /> {/* Pastikan MahasiswaCard menerima prop user */}
+          </TinderCard>
+        ))}
       </div>
+      <ToastContainer/>
     </div>
 );
 }
--- src/modules/MatchingModule/module-elements/MahasiswaCard.tsx
+++ src/modules/MatchingModule/module-elements/MahasiswaCard.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Triangle, GraduationCap, Users, Award, MessageCircleHeart } from 'lucide-react';
+import { Triangle, GraduationCap, Users, Award, MessageCircleHeart, Clock, CircleUserRound } from 'lucide-react';
+import { MahasiswaType, UserType } from "../interface";
+import 'react-toastify/dist/ReactToastify.css';
 
-function MahasiswaCard() {
+function MahasiswaCard({user} : {user:UserType}) {
   const [isOpen, setOpen] = useState(false);
   const toggleOpen = () => {
     setOpen(!isOpen)
@@ -11,42 +13,45 @@ function MahasiswaCard() {
     <div>
       <div className="flex flex-col text-black rounded-2xl w-[650px] relative shadow-xl">
         <div className="bg-blue-300 rounded-t-2xl h-28 px-6 py-3 flex justify-end">
-          <p className="max-w-[110px] text-left">Universitas Indonesia</p>
+          {/* <p className="max-w-[110px] text-left">Universitas Indonesia</p> */}
         </div>
         <div className="bg-[#EFEFEF] relative p-6 z-20 flex gap-7 shadow-xl rounded-b-2xl">
           <div className="relative w-2/5">
-            <div className="w-32 h-32 bg-gray-400 absolute top-[-80px]"></div>
+            {/* <div className="w-32 h-32 bg-gray-400 absolute top-[-80px]"></div> */}
+            <CircleUserRound size={140} className="text-white bg-gray-400 absolute top-[-80px]"/>
           </div>
           <div className="flex flex-col justify-start gap-3">
             <div>
               <div className="flex gap-6 items-center">
-                <p className="text-blue-600 font-bold text-xl">Azmy Naufal Athailah</p>
-                <p className="px-2 text-white text-sm bg-blue-300 rounded-3xl">2022</p>
+                <p className="text-blue-600 font-bold text-xl">{user.mahasiswa?.nama}</p>
+                <p className="px-2 text-white text-sm bg-blue-300 rounded-3xl">{user.mahasiswa?.angkatan}</p>
               </div>
               <div className="flex gap-6 items-center">
-                <p className="font-bold text-blue-300">azmynaufal</p>
-                <p className="text-xs text-gray-500">Terakhir aktif 4j lalu</p>
+                <p className="font-bold text-blue-300">{user.email}</p>
+                <p className="text-xs text-gray-500">
+                  <div className="flex gap-1 items-center "><Clock size={15} className="text-blue-300"/><p>Terakhir aktif 4j lalu</p></div>
+                </p>
               </div>
             </div>
-            <p className="text-sm text-blue-600">Universitas Indonesia - Ilmu Komputer</p>
+            <p className="text-sm text-blue-600">{`${user.mahasiswa?.asal_universitas} - ${user.mahasiswa?.jurusan}`}</p>
             <p className="text-sm">Cek kumpulan bio Instagram aesthetic bahasa Inggris singkat dan artinya, romantis, lucu, dan Islami terbaru 2023.</p>
             <div className="flex justify-end w-full">
               <div className="rounded-3xl">
-                <Triangle className={`text-blue-300 hover:cursor-pointer ${isOpen ? "rotate-180": ""}`}
+                <Triangle className={`text-blue-300 hover:cursor-pointer ${isOpen ? "": "rotate-180"}`}
                 onClick={toggleOpen}/>
               </div>
             </div>
           </div>
         </div>
       </div>
-      { isOpen && (<div className="px-10 py-6 flex justify-between w-[650px] absolute -bottom-36 text-sm bg-[#EFEFEF] shadow-xl rounded-b-2xl">
+      { isOpen && (<div className="px-10 py-6 flex justify-between w-[650px] absolute -bottom-[160px] text-sm bg-[#EFEFEF] shadow-xl rounded-b-2xl">
         <div className="flex flex-col gap-3 max-w-[30%]">
           <div className="flex flex-col gap-1">
             <div className="text-blue-600 flex items-center gap-2 font-semibold">
               <GraduationCap size={20}/>
               <p>Alumni SMA</p>
             </div>
-            <p className="text-xs">SMA Pradita Dirgantara</p>
+            <p className="text-xs">{`SMAN 2 Cibinong`}</p>
           </div>
           <div className="flex flex-col gap-1">
             <div className="text-blue-600 flex items-center gap-2 font-semibold">
@@ -54,8 +59,11 @@ function MahasiswaCard() {
               <p>Minat</p>
             </div>
             <ul className="list-disc list-inside text-xs">
-              <li>Kamu</li>
-              <li>Bahasa Perancis</li>
+            {user.mahasiswa?.minat_mahasiswa.map((minat, index) => (
+              <li key={index}>
+                {minat.minat}
+              </li>
+            ))}
             </ul>
           </div>
         </div>
@@ -66,8 +74,11 @@ function MahasiswaCard() {
               <p>Prestasi</p>
             </div>
             <ul className="list-disc list-inside text-xs">
-              <li>Juara 1 Hackathon FindIT UGM</li>
-              <li>Runner Up Data Analytics Dash COMPFEST 2023</li>
+            {user.mahasiswa?.pencapaian_mahasiswa.map((prestasi, index) => (
+              <li key={index}>
+                {prestasi.nama_pencapaian} {prestasi.tahun}
+              </li>
+            ))}
             </ul>
           </div>
         </div>
@@ -78,8 +89,11 @@ function MahasiswaCard() {
               <p>Organisasi</p>
             </div>
             <ul className="list-disc list-inside text-xs">
-              <li>Ketua BEM Fakultas Ilmu Komputer UI 2019</li>
-              <li>PIC Event COMPFEST 2020</li>
+            {user.mahasiswa?.organisasi_mahasiswa.map((organisasi, index) => (
+              <li key={index}>
+                {organisasi.jabatan} {organisasi.nama_organisasi} {organisasi.mulai_masa_jabatan}
+              </li>
+            ))}
             </ul>
           </div>
         </div>
